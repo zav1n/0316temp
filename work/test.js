509e43c67400e5c7472c9d3c9fac5bdf312d1582
zav1n <1114370611@qq.com>
2020-05-11 10:10:15 +0800

    0

--- work/test.js
+++ work/test.js
@@ -311,3 +311,29 @@ console.log(flatArr(arr)) +
   })();
 console.log(d);
 console.log(c);
+
+// let list = [
+// {id:1,name:"部门A",parentId:0},
+// {id:2,name:"部门B",arentId:0},
+// {id:3,name:"部门C",parentId:1},
+// {id:4,name:"部门D",parentId:1},
+// {id:5,name:"部门E",parentId:2},
+// {id:6,name:"部门F",parentId:3},
+// {id:7,name:"部门G",parentId:2},
+// {id:8,name:"部门H",parentId:4},
+// ];
+// const result = convert(list,...);
+// 转换后的结果如下
+//  let result = [
+// {
+// id:1,
+// name:"部门A",
+// parentId:0,
+// children:[
+
+//   {
+//   id:3,
+//   name:"部门C",
+//   parentId:1,
+//   children:[
+//   }
